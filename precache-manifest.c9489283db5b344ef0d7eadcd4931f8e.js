23767b2f6585c8c3d7162980f3e9b140b205bc87
Barish Arslan <barisharslan42@gmail.com>
2019-11-17 22:00:46 -0800

    Updates

---
+++ precache-manifest.c9489283db5b344ef0d7eadcd4931f8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c40c6f67bbbed6c256925a79796092ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79dd5e0370fbbfe6ecd9",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "105f0f78c9a38c26370a",
+    "url": "/static/js/2.b73ba694.chunk.js"
+  },
+  {
+    "revision": "79dd5e0370fbbfe6ecd9",
+    "url": "/static/js/main.7655b83e.chunk.js"
+  },
+  {
+    "revision": "5ad690dc0513fbca1905",
+    "url": "/static/js/runtime-main.56fc3318.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
